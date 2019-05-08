8849ddbbd47eab9bd8717c07249dda30a50ffd2e
Woody <bastholm.casper@gmail.com>
2019-05-08 11:26:34 +0200

    Made so it shows the recipes on the home page

--- RecipeManager/Pages/Index.cshtml.cs
+++ RecipeManager/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess;
+using Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,17 @@ namespace RecipeManager.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<Recipe> Recipes { get; set; }
+        private RecipeRepository recipeRepository;
+
+        public IndexModel()
+        {
+            recipeRepository = new RecipeRepository();
+        }
+
         public void OnGet()
         {
-
+            Recipes = recipeRepository.GetAllRecipes();
         }
     }
 }
